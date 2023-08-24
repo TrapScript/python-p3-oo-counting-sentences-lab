1a52a824c31435f2fd1748651a864f24269ca7f4
Neon Ratemo <neon.ratemo@student.moringaschool.com>
2023-08-24 16:23:45 +0300

    done

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+    def __init__(self, value):
+        if isinstance(value, str):
+            self._value = value
+        else:
+            raise ValueErrror("Value must be a string")
+  
